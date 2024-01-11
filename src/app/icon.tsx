c5da25cb64d5ed695e125db66effbbb18c6fdec2
sangmin <sangmin4208@gmail.com>
2024-01-11 16:42:00 +0900

    🎨 meta data setup

---
+++ src/app/icon.tsx
@@ -0,0 +1,28 @@
+import { Shapes } from "lucide-react";
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <Shapes />
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
+}
